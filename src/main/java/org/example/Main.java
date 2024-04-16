d01d3966781ee77892a8f6c6f3ff75836afeafd7
ddealsan <ddealsan@myuax.com>
2024-04-16 11:19:08 +0200

    avanzamos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import InterfazBienvenida.InterfazBienvenida;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new InterfazBienvenida();
     }
 }
