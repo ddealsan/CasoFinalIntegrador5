c0aa297913f35b06e4d7bf1d79c345ae8f6c526a
ddealsan <ddealsan@myuax.com>
2024-04-16 23:46:09 +0200

    avanzamos

---
+++ src/main/java/GestionDeInformacion/VerArchivos.java
@@ -0,0 +1,13 @@
+package GestionDeInformacion;
+
+import javax.swing.*;
+
+public class VerArchivos extends JFrame {
+    public VerArchivos() {
+        setTitle("Ver Archivos");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Aquí puedes añadir los componentes que necesites para tu ventana
+    }
+}
